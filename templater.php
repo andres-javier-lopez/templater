103deb804bc7fd22342393f31180b25dc4af7b4e
Andrés Javier López <ajavier.lopez@gmail.com>
2013-02-07 15:51:45 -0600

    Funcionamiento básico para mostrar una plantilla asíncrona

--- templater.php
+++ templater.php
@@ -1,14 +1,34 @@
 <?php
 
+// Author: Andrés Javier López <ajavier.lopez@gmail.com>
+// Version: 0.1
+// Licenced under LGPLv3
+
 $moondragon = '../moondragon/moondragon.core.php';
+$template_dir = 'templates';
 
 require_once $moondragon;
 require_once 'moondragon.render.php';
 require_once 'moondragon.manager.php';
 
+Template::addDir($template_dir);
+
 class Templater extends Manager
 {
+	public function index() {
+		return "Thank you for using Templater";
+	}
 	
+	public function __call($template, $params) {
+		try {
+			return Template::load($template);
+		}
+		catch(TemplateNotFoundException $e) {
+			return 'Unexistent';
+		}
+	}
 }
+
+MoonDragon::run(new Templater());
 
 // Fin del archivo
