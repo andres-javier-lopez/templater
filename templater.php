5fc1276804c25cc983c66b26c68f660095ad8620
Andrés Javier López <ajavier.lopez@gmail.com>
2013-02-07 15:37:36 -0600

    Initial commit

---
+++ templater.php
@@ -0,0 +1,14 @@
+<?php
+
+$moondragon = '../moondragon/moondragon.core.php';
+
+require_once $moondragon;
+require_once 'moondragon.render.php';
+require_once 'moondragon.manager.php';
+
+class Templater extends Manager
+{
+	
+}
+
+// Fin del archivo
